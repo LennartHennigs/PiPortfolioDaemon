9129e61b866b34070c1ba58db7f0c9dd4d437f08
Lennart Hennigs <mail@lennarthennigs.de>
2023-05-01 20:10:43 +0200

    added alerts, added WS setup function

--- public/rpf.js
+++ public/rpf.js
@@ -1,49 +1,87 @@
+///////////////////////////////////////////////////////////////////////////////
+
 let debug = false;
 
 const port = 40510;
-let ws = new WebSocket(`ws://${location.hostname}:${port}`);
+
+///////////////////////////////////////////////////////////////////////////////
 
 if (typeof window.WebSocket === 'undefined') {
     alert('Websockets are not supported');
+} else {
+    let ws;
+    connectWebSocket();
 }
 
-ws.addEventListener('open', () => {
-    console.log('Connected to server!');
-    ws.send(JSON.stringify({ command: 'page_loaded' }));
-});
+///////////////////////////////////////////////////////////////////////////////
+// adds event listeners to the websockets
 
-ws.addEventListener('message', (event) => {
-    const msg = JSON.parse(event.data);
-    if (debug) console.log(msg);
+function connectWebSocket() {
+    ws = new WebSocket(`ws://${location.hostname}:${port}`);
+    ws.addEventListener('open', () => {
+        console.log('Connected to server!');
+        ws.send(JSON.stringify({ command: 'page_loaded' }));
+        initializeInputEvents(); 
+    });
 
-    switch (msg.command) {
-        case 'log':
-            updateLog(msg.data);
-            break;
-        case 'set_folder':
-            getFolder(msg.folder);
-            break;
-        case 'dir':
-            populateFileList(msg);
-            break;
-        default:
-            console.log('error');
-            break;
-    }
-});
+    ws.addEventListener('message', (event) => {
+        const msg = JSON.parse(event.data);
+        if (debug) console.log(msg);
 
-ws.addEventListener('close', () => {
-    ws = null;
-    console.log('Disconnected!!');
-    setTimeout(() => {
-        ws = new WebSocket(`ws://${location.hostname}:${port}`);
-    }, 5000);
-});
+        switch (msg.command) {
+            case 'log':
+                updateLog(msg.data);
+                break;
+            case 'set_folder':
+                getFolder(msg.folder);
+                break;
+            case 'dir':
+                populateFileList(msg);
+                break;
+            default:
+                console.log('error');
+                break;
+        }
+    });
 
+    ws.addEventListener('error', () => {
+        console.log('WebSocket error');
+        alert('Error connecting to WebSocket. The page will now reload.');
+        location.reload();
+    });
+
+
+    ws.addEventListener('close', () => {
+        ws = null;
+        console.log('Disconnected!!');
+        alert('WebSocket connection lost. The page will now reload.');
+        location.reload();
+        setTimeout(() => {
+            ws = new WebSocket(`ws://${location.hostname}:${port}`);
+        }, 5000);
+    });
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+function initializeInputEvents() {
+    document.getElementById('current_folder').addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            sendFolder();
+        }
+    });
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// reads the folder name from the text field
 function getFolder(folder) {
     document.getElementById('current_folder').value = folder;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// lists all the files for the folder
 function populateFileList({ files = [] } = {}) {
     const fileList = document.getElementById('file_list');
     fileList.innerHTML = '';
@@ -58,16 +96,22 @@ function populateFileList({ files = [] } = {}) {
     });
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// adds an entry to the upload log
 function updateLog(data) {
     document.getElementById('log').value += data;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// send a new folder string to the server
 function sendFolder() {
     const folder = document.getElementById('current_folder').value;
     console.log("SEND FOLDER");
     const message = {
         command: 'set_folder',
-        folder: folder,
+        folder,
     };
     ws.send(JSON.stringify(message));
 }
+
+///////////////////////////////////////////////////////////////////////////////
