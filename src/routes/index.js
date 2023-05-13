37dbaa37b335dfc167cc3045987fb68690a3af9d
Lennart Hennigs <mail@lennarthennigs.de>
2023-05-13 18:42:08 +0200

    now serving the logo (png)

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const { getHomePage, serveStaticFiles, downloadFile } = require('../controllers'
 const router = express.Router();
 
 router.get('/', getHomePage);
-router.get('*.(html|css|js)', serveStaticFiles);
+router.get('*.(html|css|js|png)', serveStaticFiles);
 router.get('/download/:filename', downloadFile);
 
 module.exports = router;
