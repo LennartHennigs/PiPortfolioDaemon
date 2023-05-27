62db17d40b7f35ccac84a88cff86f3e0229d2643
Lennart Hennigs <mail@lennarthennigs.de>
2023-05-27 08:16:39 +0200

    added try-catch to websockets, added drag&drop

--- public/rpf.js
+++ public/rpf.js
@@ -12,55 +12,61 @@ if (typeof window.WebSocket === 'undefined') {
     let ws;
     connectWebSocket();
 }
+enableDropZone();
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // adds event listeners to the web sockets
 
 function connectWebSocket() {
-    ws = new WebSocket(`ws://${location.hostname}:${port}`);
-    ws.addEventListener('open', () => {
-        console.log('Connected to server!');
-        ws.send(JSON.stringify({ command: 'page_loaded' }));
-        initializeInputEvents(); 
-    });
+    try {
+        ws = new WebSocket(`ws://${location.hostname}:${port}`);
+        ws.addEventListener('open', () => {
+            console.log('Connected to server!');
+            ws.send(JSON.stringify({ command: 'page_loaded' }));
+            initializeInputEvents(); 
+        });
 
-    ws.addEventListener('message', (event) => {
-        const msg = JSON.parse(event.data);
-        if (debug) console.log(msg);
+        ws.addEventListener('message', (event) => {
+            const msg = JSON.parse(event.data);
+            if (debug) console.log(msg);
 
-        switch (msg.command) {
-            case 'add_to_log':
-                updateLog(msg.data);
-                break;
-            case 'set_folder':
-                getFolder(msg.folder);
-                break;
-            case 'dir':
-                populateFileList(msg);
-                break;
-            default:
-                console.log('error');
-                break;
-        }
-    });
+            switch (msg.command) {
+                case 'add_to_log':
+                    updateLog(msg.data);
+                    break;
+                case 'set_folder':
+                    getFolder(msg.folder);
+                    break;
+                case 'dir':
+                    populateFileList(msg);
+                    break;
+                default:
+                    console.log('error');
+                    break;
+            }
+        });
 
-    ws.addEventListener('error', () => {
-        console.log('WebSocket error');
-        alert('Error connecting to WebSocket. The page will now reload.');
-        location.reload();
-    });
+        ws.addEventListener('error', () => {
+            console.log('WebSocket error');
+            alert('Error connecting to WebSocket. The page will now reload.');
+            location.reload();
+        });
 
-    ws.addEventListener('close', () => {
-        ws = null;
-        console.log('Disconnected!!');
-        alert('WebSocket connection lost. The page will now reload.');
-        location.reload();
-        setTimeout(() => {
-            ws = new WebSocket(`ws://${location.hostname}:${port}`);
-        }, 5000);
-    });
+        ws.addEventListener('close', () => {
+            ws = null;
+            console.log('Disconnected!!');
+            alert('WebSocket connection lost. The page will now reload.');
+            location.reload();
+            setTimeout(() => {
+                ws = new WebSocket(`ws://${location.hostname}:${port}`);
+            }, 5000);
+        });
+    } catch (error) {
+        console.error(error);
+        return;
+    }
 }
-
 ///////////////////////////////////////////////////////////////////////////////
 
 function initializeInputEvents() {
@@ -109,6 +115,52 @@ function sendFolder() {
         folder,
     };
     ws.send(JSON.stringify(message));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+function enableDropZone() {
+    let dropZone = document.getElementById('drop_zone');
+    let fileInput = document.getElementById('filesToUpload');
+    let maxSizeInMB = 2; // Maximum size allowed in MB
+    let maxSize = maxSizeInMB * 1024 * 1024; // Convert size to bytes
+
+    dropZone.ondragover = dropZone.ondragenter = function(evt) {
+        evt.preventDefault();
+        this.classList.add('over'); 
+    };
+
+    dropZone.ondrop = function(evt) {
+        // Check file size
+        for(let i = 0; i < evt.dataTransfer.files.length; i++) {
+            if (evt.dataTransfer.files[i].size > maxSize) {
+                alert('File size should not exceed ' + maxSizeInMB + ' MB');
+                return;
+            }
+        }
+        fileInput.files = evt.dataTransfer.files;
+        evt.preventDefault();
+        this.classList.remove('over'); 
+        
+    };
+
+    dropZone.ondragleave = function(evt) {
+        this.classList.remove('over'); 
+    };
+
+    fileInput.onchange = function() {
+        // Check file size
+        for(let i = 0; i < fileInput.files.length; i++) {
+            if (fileInput.files[i].size > maxSize) {
+                alert('File size should not exceed ' + maxSizeInMB + ' MB');
+                return;
+            }
+        }
+        let files = fileInput.files;
+        for (let i = 0; i < files.length; i++) {
+            console.log(files[i].name);
+        }
+    };
 }
 
 ///////////////////////////////////////////////////////////////////////////////
