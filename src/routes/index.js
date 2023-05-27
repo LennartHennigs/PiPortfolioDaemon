c8e8a301b98ef3b5f7e81dc7df649b8bf61638d3
Lennart Hennigs <mail@lennarthennigs.de>
2023-05-27 08:23:15 +0200

    keep uploaded file name

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,23 @@
 
 const express = require('express');
-const { getHomePage, serveStaticFiles, downloadFile } = require('../controllers');
+const multer  = require('multer');
+const { getHomePage, serveStaticFiles, downloadFile, uploadFile } = require('../controllers');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'upload/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({ storage: storage });
 const router = express.Router();
 
 router.get('/', getHomePage);
 router.get('*.(html|css|js|png)', serveStaticFiles);
 router.get('/download/:filename', downloadFile);
+router.post('/upload', upload.single('filesToUpload'), uploadFile);
 
 module.exports = router;
