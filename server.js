210112af8644799dd336cbe1df118e3a1e6356af
Lennart Hennigs <mail@lennarthennigs.de>
2023-05-28 09:04:13 +0200

    checking paths and command

--- server.js
+++ server.js
@@ -3,29 +3,47 @@ const config = require('./src/config');
 
 const { setupWebSockets } = require('./src/websockets');
 const { setupDaemon } = require('./src/daemon');
-const { getDirListFromPortfolio, getIP } = require('./src/utils');
+const { doesPathExists, isCommandAvailable, getIP } = require('./src/utils');
 
 const PORT = process.env.PORT || config.appPort;
 const args = process.argv.slice(2);
 
+if (!doesPathExists(config.webserverFolder)) {
+    throw new Error(`webserverFolder does not exist: ${config.webserverFolder}`);
+}
+if (!doesPathExists(config.homepage)) {
+    throw new Error(`homepage file does not exist: ${config.homepage}`);
+}
+if (!doesPathExists(config.sharedFolder)) {
+    throw new Error(`sharedFolder does not exist: ${config.sharedFolder}`);
+}
+if (!doesPathExists(config.tmpFolder)) {
+    throw new Error(`tmpFolder does not exist: ${config.tmpFolder}`);
+}
+if (!isCommandAvailable('rpfolio')) {
+    throw new Error('rpfolio command not found!');
+}   
+
 // Set the drive variable based on command line parameters
-config.drive = (args[0] === undefined) ? 'c:' : args[0];
+if (args[0] !== undefined) {
+    config.portfolioPath = args[0];
+}
 
 console.log();
 console.log(`${config.ID} ${config.VERSION}`);
-console.log(`-> ${config.drive}\n`);
-
-// Get directory contents from the portfolio
-console.log('Getting dir contents...');
-console.log();
-
-// console.log(config);
+console.log(`-> ${config.portfolioPath}\n`);
 
 // Start the app and set up websockets and the daemon
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
     const address = getIP();
     console.log(`Webserver running at http://${address}:${config.appPort}`);
     
     setupWebSockets();
     setupDaemon();
+});
+
+server.on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
 });
